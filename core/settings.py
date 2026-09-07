a62a0c6933d1dd82ecaa434240f9b04785a7bb36
leonelmadridsani15 <leo.madrid@duocuc.cl>
2026-09-07 14:57:05 -0300

    Autorizar dominio de Render

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cur9l5vcd+-xyh!jbb2=hm+xp6&8@#dm&wi6z)3fjphrl)(3+d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['canchasya-ylqk.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
